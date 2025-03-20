f601dd9729e49efa977d22add494aa3fe5e65e11
Podik <22stalkerpro22@gmail.com>
2025-03-20 23:01:37 +0200

    Adding Auth, Home and Admin pages

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from "../components/Header.jsx";
+import AuthForm from "../components/AuthForm.jsx";
+
+const Auth = () => {
+    return (
+        <>
+            <Header/>
+            <AuthForm/>
+        </>
+    );
+};
+
+export default Auth;
